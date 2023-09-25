edc3bff7236a255a4b25895866a2c459efd4fede
JiAn-Lim <limjian1990@gmail.com>
2023-09-25 20:29:41 +0900

    Feat: API author, post
    Add POST, GET, DELETE, UPDATE

--- api/controller/author_controller.go
+++ api/controller/author_controller.go
@@ -1,25 +1,139 @@
 package controller
 
 import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/LimJiAn/gin-sqlboiler-exam/database"
+	"github.com/LimJiAn/gin-sqlboiler-exam/models"
 	"github.com/gin-gonic/gin"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func GetAuthors(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	authors, err := models.Authors().All(ctx, database.DB)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, authors)
 }
 
 func GetAuthor(c *gin.Context) {
+	id := c.Param("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	author, err := models.FindAuthor(ctx, database.DB, authorId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, author)
 }
 
 func NewAuthor(c *gin.Context) {
+	var author models.Author
+	if err := c.ShouldBind(&author); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := author.Insert(ctx, database.DB, boil.Infer()); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, author)
 }
 
 func DeleteAuthor(c *gin.Context) {
+	id := c.Param("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	author, err := models.FindAuthor(ctx, database.DB, authorId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if _, err := author.Delete(ctx, database.DB); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, author)
 
 }
 
 func UpdateAuthor(c *gin.Context) {
+	id := c.Param("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
+	newAuthor := models.Author{}
+	if err := c.ShouldBind(&newAuthor); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	author, err := models.FindAuthor(ctx, database.DB, authorId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	author.Name = newAuthor.Name
+	if _, err := author.Update(ctx, database.DB, boil.Infer()); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, author)
 }
--- api/controller/post_contoller.go
+++ api/controller/post_contoller.go
@@ -1,25 +1,141 @@
 package controller
 
 import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/LimJiAn/gin-sqlboiler-exam/database"
+	"github.com/LimJiAn/gin-sqlboiler-exam/models"
 	"github.com/gin-gonic/gin"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func GetPosts(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
+	posts, err := models.Posts().All(ctx, database.DB)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, posts)
 }
 
 func GetPost(c *gin.Context) {
+	id := c.Param("id")
+	postId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	post, err := models.FindPost(ctx, database.DB, postId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, post)
 
 }
 
 func NewPost(c *gin.Context) {
+	var post models.Post
+	if err := c.ShouldBind(&post); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := post.Insert(ctx, database.DB, boil.Infer()); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, post)
 
 }
 
 func DeletePost(c *gin.Context) {
+	id := c.Param("id")
+	postId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	post, err := models.FindPost(ctx, database.DB, postId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if _, err := post.Delete(ctx, database.DB); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, post)
 
 }
 
 func UpdatePost(c *gin.Context) {
+	id := c.Param("id")
+	postId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	newPost := models.Post{}
+	if err := c.ShouldBind(&newPost); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	post, err := models.FindPost(ctx, database.DB, postId)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	post.Title = newPost.Title
+	if _, err := post.Update(ctx, database.DB, boil.Infer()); err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, post)
 
 }
